5b251715c99e7d48e93ca5f38da8695bb3ea4b1a
saif-saleem <133539527+saif-saleem@users.noreply.github.com>
2023-09-21 00:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
+getSumBtn.textContent = "Get Total Price"; // Use textContent to set button text
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  let price = document.getElementsByClassName("price");
+  let s = 0;
+
+  	for (let k of price) {
+		s=s+parseInt(k.innerText);
+	}
+
+  let row = document.createElement("tr");
+  row.innerHTML = `<td>total value</td> <td>${s}</td>`;
+  let tb = document.getElementsByTagName("table")[0];
+  tb.appendChild(row);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
